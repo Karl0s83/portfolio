cfcd44c0a492c082995fdd6964b5cff4f76d0ac8
Karl0s83 <estikarlos@gmail.com>
2025-11-26 11:34:41 +0100

    minor fixes, add skills section, need change skills background and button

--- funciones.js
+++ funciones.js
@@ -1,45 +1,33 @@
-/* Esta función está dirigida al texto debajo de la foto*/
+//close the menu sistem when clicking a link
+document.querySelectorAll("#menu a").forEach(link => {
+  link.addEventListener("click", () => {
 
-document.addEventListener("DOMContentLoaded", function () {
-  const text = "AWS Cloud | UI/UX Designer ";
-  const target = document.getElementById("typing");
-  if (!target) return;
-  let i = 0;
+    const checkbox = document.querySelector("#menu-checkbox");
 
-  function typeWriter() {
-    if (i < text.length) {
-      target.innerHTML += text.charAt(i);
-      i++;
-      setTimeout(typeWriter, 100);
-    }
-  }
-
-  typeWriter();
+    checkbox.checked = false;
+    setTimeout(() => {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }, 300);
+  });
 });
 
+// sistem tabs
+const tabs = document.querySelectorAll(".tab");
+const panels = document.querySelectorAll(".panel");
 
-/* Esta función es para el cambio de idioma */
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
 
-let currentLang = 'es';
+    tabs.forEach(t => t.classList.remove("active"));
+    tab.classList.add("active");
 
-function toggleLanguage() {
-  if (currentLang === 'es') {
-    currentLang = 'en';
-    document.getElementById('lang-toggle').textContent = 'ES';
-    // Aquí cambia a inglés
-    // loadEnglishTexts();
-  } else {
-    currentLang = 'es';
-    document.getElementById('lang-toggle').textContent = 'EN';
-    // Aquí cambia a español
-    // loadSpanishTexts();
-  }
-}
+    panels.forEach(panel => panel.hidden = true);
+    const target = document.getElementById(tab.getAttribute("aria-controls"));
+    target.hidden = false;
+  });
+});
 
+//language button
 function setLanguage(lang) {
-  currentLang = lang;
-  document.getElementById('lang-toggle').textContent = lang === 'es' ? 'EN' : 'ES';
-  // Aquí cambiarías el contenido según lang
-  // if (lang === 'es') { loadSpanishTexts(); }
-  // else { loadEnglishTexts(); }
+  alert("Idioma cambiado a: " + lang);
 }
